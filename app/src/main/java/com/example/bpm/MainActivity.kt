67816375eac674b5b1cece20e66c1f552c861696
eseabro <62852911+eseabro@users.noreply.github.com>
2023-02-11 16:32:31 -0500

    spotify code needs to be tested

--- app/src/main/java/com/example/bpm/MainActivity.kt
+++ app/src/main/java/com/example/bpm/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.bpm
-
-import android.app.DownloadManager
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import com.beust.klaxon.Klaxon
 import android.os.Bundle
+import kotlinx.coroutines.*
+import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
-import com.spotify.android.appremote.api.SpotifyAppRemote;
-
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
-import com.spotify.protocol.types.Track;
+import androidx.lifecycle.lifecycleScope
+import com.google.gson.internal.bind.TypeAdapters.URL
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.types.Track
 import org.json.JSONArray
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val clientId = "4073955ddd87480fa0ab26ec705705e9"
+    private val redirectUri = "https://com.spotify.android.BPMile/callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+    private val client: OkHttpClient = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,70 +34,6 @@ class MainActivity : AppCompatActivity() {
         val runningSpeedDial = findViewById<SeekBar>(R.id.runningSpeedDial)
         val genreSelect = findViewById<Spinner>(R.id.genreSelect)
 
-
-        fun spotifyConnect(){
-            private val clientId = "4073955ddd87480fa0ab26ec705705e9"
-            private val redirectUri = "https://com.spotify.android.bpm/callback"
-            private var spotifyAppRemote: SpotifyAppRemote? = null
-            private val client = OkHttpClient()
-            val connectionParams = ConnectionParams.Builder(clientId)
-                .setRedirectUri(redirectUri)
-                .showAuthView(true)
-                .build()
-
-            SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
-                override fun onConnected(appRemote: SpotifyAppRemote) {
-                    spotifyAppRemote = appRemote
-                    Log.d("MainActivity", "Connected! Yay!")
-                    // Now you can start interacting with App Remote
-                    connected()
-                }
-
-                override fun onFailure(throwable: Throwable) {
-                    Log.e("MainActivity", throwable.message, throwable)
-                    // Something went wrong when attempting to connect! Handle errors here
-                }
-                private fun connected() {
-                    spotifyAppRemote?.let {
-                        // Play a playlist
-                        val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-                        it.playerApi.play(playlistURI)
-                        // Subscribe to PlayerState
-                        it.playerApi.subscribeToPlayerState().setEventCallback {
-                            val track: Track = it.track
-                            Log.d("MainActivity", track.name + " by " + track.artist.name)
-                        }
-                    }
-
-                }
-                fun get_recs(url: String): JSONArray{
-                    spotifyAppRemote?.let {
-                        val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-                        it.playerApi.play(playlistURI)
-                        it.playerApi.subscribeToPlayerState().setEventCallback {
-                            val track: Track = it.track
-                            Log.d("MainActivity", track.name + " by " + track.artist.name)
-                        }
-                    }
-
-                }
-
-            })
-
-        }
-
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.spinner_options,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            // Specify the layout to use when the list of choices appears
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            // Apply the adapter to the spinner
-//            genreSelect.adapter = adapter
-//        }
-
-
         createPlaylist.setOnClickListener {
             val mph: Double = runningSpeed.text.toString().toDouble();
             val genre =  genreSelect.getSelectedItem().toString();
@@ -95,13 +41,52 @@ class MainActivity : AppCompatActivity() {
                 return (1.456876456876459 * mph * mph) - (42.027972027972076 * mph) + 438.99766899766934
             }
             val BPM = mphToBpm(mph)
-//            val recommendedSongs = spotify.Get
 
-            // TO DO:
-            // - Get reccommended songs based on genre and tempo
-            // - Create a playlist
-            // - Add recommended songs to playlist
-            // - Sends use to playlist in spotify app
         }
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                getPlaylist()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+            }
+        })
+    }
+    private fun getPlaylist() {
+
+        fun getRequest(sUrl: String): String? {
+            var result: String? = null
+            try {
+                val url = URL(sUrl)
+                val request = Request.Builder().url(url).build()
+                val response = client.newCall(request).execute()
+                result = response.body?.string()
+            } catch (err: Error) {
+                print("Error when executing get request: " + err.localizedMessage)
+            }
+            return result
+        }
+
+    }
+    override fun onStop() {
+        super.onStop()
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
+
     }
 }
