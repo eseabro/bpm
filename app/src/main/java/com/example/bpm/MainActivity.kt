bfa9cbc610b73c480b0e5522823dc572c0731e94
AndresA-S <acklandsnow01@gmail.com>
2023-02-11 21:56:34 -0500

    sending user to spotify playlist on click

--- app/src/main/java/com/example/bpm/MainActivity.kt
+++ app/src/main/java/com/example/bpm/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.bpm
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.SeekBar
+import android.widget.Spinner
 import android.widget.TextView
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import kotlin.math.*
 
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     private val clientId = "cb9bed781f1b413e8878a3e93fa3da4a"
     private val redirectUri = "http://bpm-app.com/callback/"
     private var spotifyAppRemote: SpotifyAppRemote? = null
+
+    val playlists = JSONObject("""{
+        "190.0": "https://open.spotify.com/playlist/37i9dQZF1EIcID9rq1OAoH?si=1a26bce7353b4197",
+        "180.0": "https://open.spotify.com/playlist/37i9dQZF1EIerWUrjG2OiJ?si=8232f95c918d4090",
+        "170.0": "https://open.spotify.com/playlist/37i9dQZF1EIgfIackHptHl?si=e44d63f43b084e71",
+        "160.0": "https://open.spotify.com/playlist/37i9dQZF1EIdYV92VKrjuC?si=67a53f7157a3434f",
+        "150.0": "https://open.spotify.com/playlist/37i9dQZF1EIgrZKdA44WQK?si=9fb005947d5e44dd",
+        "140.0": "https://open.spotify.com/playlist/37i9dQZF1EIgOKtiospcqN?si=dc13d60f07fa44f8"
+    }""")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +43,33 @@ class MainActivity : AppCompatActivity() {
         val runningSpeedDial = findViewById<SeekBar>(R.id.runningSpeedDial)
 
         createPlaylist.setOnClickListener {
-            connected()
+            val mph: Double = runningSpeed.text.toString().toDouble();
+
+            fun mphToBpm(mph: Double): Double {
+                return (-0.35174437271856374 * mph * mph +  27.50030269012755 * mph + 38.98561871213088)
+            }
+
+            val BPM = Math.round(mphToBpm(mph) / 10.0) * 10.0;
+
+            // take BPM and return "playlist" JSONobject key
+            var playlistLink = playlists.get(BPM.toString());
+
+            val i = Intent(Intent.ACTION_VIEW);
+            i.data = Uri.parse(playlistLink.toString());
+            startActivity(i);
+
+
+            // nothing below 140
+            // nothing above 190
+
+            // TO DO:
+            // - Get miles per hour
+            // - Get the genre
+            // - Get tempo using miles per hour
+            // - Get reccommended songs based on genre and tempo
+            // - Create a playlist
+            // - Add recommended songs to playlist
+            // - Sends use to playlist in spotify app
         }
 
         runningSpeedDial.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -43,7 +83,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 // TODO Auto-generated method stub
-                runningSpeed.text = (progress.toFloat()/5).toString()
+                runningSpeed.text = ((progress.toFloat()/10)+4).toString()
             }
         })
     }
@@ -66,17 +106,6 @@ class MainActivity : AppCompatActivity() {
                 // Something went wrong when attempting to connect! Handle errors here
             }
         })
-    }
-
-    private fun connected() {
-        // TO DO:
-        // - Get miles per hour
-        // - Get the genre
-        // - Get tempo using miles per hour
-        // - Get reccommended songs based on genre and tempo
-        // - Create a playlist
-        // - Add recommended songs to playlist
-        // - Sends use to playlist in spotify app
     }
 
     override fun onStop() {
